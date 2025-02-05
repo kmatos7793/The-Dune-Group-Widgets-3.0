3bb59cc5c59b357f9e1d8b240a763a83c982ca18
kmatos7793 <88529154+kmatos7793@users.noreply.github.com>
2025-02-05 15:48:57 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "0022509700036919";
+  let ProductId = "";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
